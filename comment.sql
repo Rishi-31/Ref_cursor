96c3ad66d49eec2e0286b42c4a5e191d04369f48
Rishikaran K <143609439+Rishi-31@users.noreply.github.com>
2025-12-06 21:34:45 +0530

    Update comment.sql

--- comment.sql
+++ comment.sql
@@ -1,7 +1,7 @@
-create or replace procedure p1(inp1 in nnumber,inp out sys_refcursor)
+create or replace procedure p1(inp1 in nnumber,dept_name in varchar,inp out sys_refcursor)
 begin
 open inp for 
 select emp_id, emp_name from emp 
-where dept_id = inp1;
+where dept_id = inp1 and dept_name = dept_name;
 end;
 /
